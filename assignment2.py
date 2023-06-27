3acf77cfa3adb6fe41304b45bafcdbc32f84c20f
ishanisawant <137489710+ishanisawant@users.noreply.github.com>
2023-06-27 23:21:49 +0530

    Add files via upload

---
+++ assignment2.py
@@ -0,0 +1,76 @@
+# Arithmetic operators
+a = 10
+b = 3
+
+Add = a + b
+Sub = a - b
+Mul = a * b
+divi = a / b
+floor_divi = a // b
+modulus = a % b
+exp = a**b
+
+print("Add:", Add)
+print("Sub:", Sub)
+print("Multiplication:", Mul)
+print("Division:", divi)
+print("Floor Division:", floor_divi)
+print("Modulus:", modulus)
+print("Exponentiation:", exp)
+
+# Comparison operators
+x = 5
+y = 7
+
+print("x > y: ", x > y)
+print("x < y: ", x < y)
+print("x >= y: ", x >= y)
+print("x <= y: ", x <= y)
+print("x == y: ", x == y)
+print("x != y: ", x != y)
+
+# Logical operators
+p = True
+q = False
+
+print("p and q: ", p and q)
+print("p or q: ", p or q)
+print("not p: ", not p)
+
+# Assignment operators
+x = 10
+y = 5
+
+x += y
+print("x after Add:", x)
+
+x -= y
+print("x after sub:", x)
+
+x *= y
+print("x after multiplication:", x)
+
+x /= y
+print("x after division:", x)
+
+x %= y
+print("x after modulus:", x)
+
+x **= y
+print("x after exponent:", x)
+
+# Bitwise operators
+p = 5
+q = 3
+
+And = p & q
+Or = p | q
+Xor = p ^ q
+Left_shift = p << 2
+Right_shift = p >> 1
+
+print("AND:", And)
+print("OR:", Or)
+print("XOR:", Xor)
+print("Left Shift:", Left_shift)
+print("Right Shift:", Right_shift)
